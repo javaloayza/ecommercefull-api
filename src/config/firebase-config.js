48b2a0544d6de3237f8bdde984dc15c001f05580
Javadeveloper7 <95065154+Javadeveloper7@users.noreply.github.com>
2023-01-11 20:41:37 -0500

    firebase-configok

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,11 +1,22 @@
 const admin = require('firebase-admin')
+require('dotenv').config()
 
 /* Importing the service account credentials from the serviceAccount.json file. */
-const serviceAccount = require('./serviceAccount1.json')
+// const serviceAccount = require('./serviceAccount1.json')
 
 /* Initializing the firebase app with the service account credentials. */
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: process.env.PRIVATE_KEY
+  })
 })
 
 module.exports = admin
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// })
+
+// module.exports = admin
