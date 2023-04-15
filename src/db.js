9c0a2d7732847bc7f8c0d1d22d08523de976ecd8
Javadeveloper7 <95065154+Javadeveloper7@users.noreply.github.com>
2023-04-15 00:49:36 -0500

    db-name

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ const {
 } = process.env
 
 /* Creating a database url to connect to the database. */
-const DB_URL = process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/ecommercefull`
+const DB_URL = process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/ecommercefull_n7wy`
 // ecommerce1`  **local database
 
 /* Creating a new instance of Sequelize and connecting to the database. */
