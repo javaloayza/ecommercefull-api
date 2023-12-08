38a3d52ead47b7090697bddc85dccac0cdcd1ab5
javaloayza <java.loayza@gmail.com>
2023-12-07 23:27:33 -0500

    upload-december

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ const {
 } = process.env
 
 /* Creating a database url to connect to the database. */
-const DB_URL = process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/remotedb_fcks`
+const DB_URL = process.env.DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/remotedb_1`
 // ecommerce1`  **local database
 
 /* Creating a new instance of Sequelize and connecting to the database. */
